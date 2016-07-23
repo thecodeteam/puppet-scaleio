c09c7963dd7568b5d79e5ae497a277cae89a9d58
Andrey Pavlov <andrey-mp@yandex.ru>
2016-07-23 12:36:51 +0300

    add 'ensure_properties' parameter to sdc_server to allows cleanup mdm_ips

--- manifests/sdc_server.pp
+++ manifests/sdc_server.pp
@@ -1,9 +1,10 @@
 # Configure ScaleIO SDC service installation
 
 class scaleio::sdc_server (
-  $ensure  = 'present',  # present|absent - Install or remove SDC service
-  $mdm_ip  = undef,      # string - List of MDM IPs
-  $ftp     = 'default'   # string - 'default' or FTP with user and password
+  $ensure               = 'present',  # present|absent - Install or remove SDC service
+  $mdm_ip               = undef,      # string - List of MDM IPs
+  $ftp                  = 'default'   # string - 'default' or FTP with user and password
+  $ensure_properties    = 'present',  # present|absent - Add or remove SDS properties
   )
 {
   $sdc_package = $::osfamily ? {
@@ -25,28 +26,32 @@ class scaleio::sdc_server (
   }
 
   if $mdm_ip != undef and $mdm_ip != '' {
-    $ip_array = split($mdm_ip, ',')
-    if $ensure == 'present' {
-      scaleio::add_ip { $ip_array:
+    if $ensure_properties == 'present' {
+      $ip_array = split($mdm_ip, ',')
+      if $ensure == 'present' {
+        scaleio::add_ip { $ip_array:
+          require => Package[$sdc_package],
+        }
+      }
+      file_line { 'Set MDM IP addresses in drv_cfg.txt':
+        ensure  => present,
+        line    => "mdm ${mdm_ip}",
+        path    => '/bin/emc/scaleio/drv_cfg.txt',
+        match   => '^mdm .*',
         require => Package[$sdc_package],
       }
     }
-    file_line { 'Set MDM IP addresses in drv_cfg.txt':
-      ensure  => present,
-      line    => "mdm ${mdm_ip}",
-      path    => '/bin/emc/scaleio/drv_cfg.txt',
-      match   => '^mdm .*',
-      require => Package[$sdc_package],
-    }
   } else {
-    file_line { 'Reset MDM IP addresses in drv_cfg.txt':
-      ensure  => absent,
-      line    => '',
-      path    => '/bin/emc/scaleio/drv_cfg.txt',
-      match   => '^mdm .*',
-      match_for_absence => true,
-      require => Package[$sdc_package],
-      replace => false,
+    if $ensure_properties == 'absent' {
+      file_line { 'Reset MDM IP addresses in drv_cfg.txt':
+        ensure  => absent,
+        line    => '',
+        path    => '/bin/emc/scaleio/drv_cfg.txt',
+        match   => '^mdm .*',
+        match_for_absence => true,
+        require => Package[$sdc_package],
+        replace => false,
+      }
     }
   }
 
