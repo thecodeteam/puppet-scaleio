24b2822095f34429f9d4729e43ee1be99e023c22
tikitavi <rtikitavi@gmail.com>
2016-08-08 16:55:53 +0400

    fix defines' tests (#13)
    * fixed tests cluster, cmd, login, sdc, mdm, protection_domain, storage_pool
    
    * removed commented code

--- spec/defines/scaleio_cluster_spec.rb
+++ spec/defines/scaleio_cluster_spec.rb
@@ -12,7 +12,7 @@ describe 'scaleio::cluster' do
       default_params.merge({ :cluster_mode => '1' })
     end
     it 'switches cluster mode' do
-      is_expected.to contain_scaleio__cmd('switch cluster mode').with(
+      is_expected.to contain_scaleio__cmd('switch cluster mode present').with(
         :action => 'switch_cluster_mode',
         :ref => 'cluster_mode',
         :value => '1_node',
@@ -21,17 +21,17 @@ describe 'scaleio::cluster' do
     end
     it { is_expected.to contain_exec('scli  --approve_certificate --switch_cluster_mode --cluster_mode 1_node   --add_slave_mdm_name  --add_tb_name  --i_am_sure').with(
       :command => 'scli  --approve_certificate --switch_cluster_mode --cluster_mode 1_node   --add_slave_mdm_name  --add_tb_name  --i_am_sure',
-      :path => '/bin/',
-      :unless => 'scli  --approve_certificate  --query_cluster | grep -A 1 "Cluster:" | grep 1_node')}
+      :path => ['/bin/'],
+      :unless => 'scli  --approve_certificate --query_cluster | grep -A 1 "Cluster:" | grep 1_node ')}
     it { is_expected.to contain_notify('SCLI COMMAND: scli  --approve_certificate --switch_cluster_mode --cluster_mode 1_node   --add_slave_mdm_name  --add_tb_name  --i_am_sure')}
-    it { is_expected.to contain_notify('SCLI UNLESS: scli  --approve_certificate  --query_cluster | grep -A 1 "Cluster:" | grep 1_node')}
+    it { is_expected.to contain_notify('SCLI UNLESS: scli  --approve_certificate --query_cluster | grep -A 1 "Cluster:" | grep 1_node ')}
 
     context 'when ensure is absent' do
       let :params do
         default_params.merge({ :ensure => 'absent', :cluster_mode => '1' })
       end
       it 'switchs cluster mode' do
-        is_expected.to contain_scaleio__cmd('switch cluster mode').with(
+        is_expected.to contain_scaleio__cmd('switch cluster mode absent').with(
           :action => 'switch_cluster_mode',
           :ref => 'cluster_mode', 
           :value => '1_node',
@@ -40,10 +40,10 @@ describe 'scaleio::cluster' do
       end
       it { is_expected.to contain_exec('scli  --approve_certificate --switch_cluster_mode --cluster_mode 1_node   --remove_slave_mdm_name  --remove_tb_name  --i_am_sure').with(
         :command => 'scli  --approve_certificate --switch_cluster_mode --cluster_mode 1_node   --remove_slave_mdm_name  --remove_tb_name  --i_am_sure',
-        :path => '/bin/',
-        :unless => 'scli  --approve_certificate  --query_cluster | grep -A 1 "Cluster:" | grep 1_node')}
+        :path => ['/bin/'],
+        :unless => 'scli  --approve_certificate --query_cluster | grep -A 1 "Cluster:" | grep 1_node ')}
       it { is_expected.to contain_notify('SCLI COMMAND: scli  --approve_certificate --switch_cluster_mode --cluster_mode 1_node   --remove_slave_mdm_name  --remove_tb_name  --i_am_sure')}   
-      it { is_expected.to contain_notify('SCLI UNLESS: scli  --approve_certificate  --query_cluster | grep -A 1 "Cluster:" | grep 1_node')}
+      it { is_expected.to contain_notify('SCLI UNLESS: scli  --approve_certificate --query_cluster | grep -A 1 "Cluster:" | grep 1_node ')}
     end
   end
   context 'when cluster_mode is undefined' do
@@ -67,50 +67,14 @@ describe 'scaleio::cluster' do
     end
     it { is_expected.to contain_exec('scli   --set_password --new_password new_password   ').with(
       :command => 'scli   --set_password --new_password new_password   ',
-      :path => '/bin/',)}
+      :path => ['/bin/'],)}
     it { is_expected.to contain_notify('SCLI COMMAND: scli   --set_password --new_password new_password   ')}
   end
   context 'when new_password isnt defined' do
     let :params do
-      default_params.merge({ :new_password => '' })
+     default_params.merge({ :new_password => nil })
     end
-    it {is_expected.not_to contain_scaleio__cmd('set password')}
-  end
-  context 'when restricted_sdc_mode is enabled' do
-    let :params do
-      default_params.merge({ :restricted_sdc_mode => 'enabled' })
-    end
-    it 'sets restricted sdc mode' do
-      is_expected.to contain_scaleio__cmd('set restricted sdc mode').with(
-        :action => 'set_restricted_sdc_mode',
-        :ref => 'restricted_sdc_mode',
-        :value => 'enabled')
-    end
-    it { is_expected.to contain_exec('scli  --approve_certificate --set_restricted_sdc_mode --restricted_sdc_mode enabled   ').with(
-      :command => 'scli  --approve_certificate --set_restricted_sdc_mode --restricted_sdc_mode enabled   ',
-      :path => '/bin/',)}
-    it { is_expected.to contain_notify('SCLI COMMAND: scli  --approve_certificate --set_restricted_sdc_mode --restricted_sdc_mode enabled   ')}
-  end
-  context 'when restricted_sdc_mode is disabled' do
-    let :params do
-      default_params.merge({ :restricted_sdc_mode => 'disabled' })
-    end
-    it 'sets restricted sdc mode' do
-      is_expected.to contain_scaleio__cmd('set restricted sdc mode').with(
-        :action => 'set_restricted_sdc_mode',
-        :ref => 'restricted_sdc_mode', 
-        :value => 'disabled')
-    end
-    it { is_expected.to contain_exec('scli  --approve_certificate --set_restricted_sdc_mode --restricted_sdc_mode disabled   ').with(
-      :command => 'scli  --approve_certificate --set_restricted_sdc_mode --restricted_sdc_mode disabled   ',
-      :path => '/bin/',)}
-    it { is_expected.to contain_notify('SCLI COMMAND: scli  --approve_certificate --set_restricted_sdc_mode --restricted_sdc_mode disabled   ')}
-  end
-  context 'when restricted_sdc_mode isnot defined' do
-    let :params do
-      default_params.merge({ :restricted_sdc_mode => '' })
-    end
-    it { is_expected.not_to contain_scaleio__cmd('set restricted sdc mode')}
+    it { is_expected.not_to contain_scaleio__cmd('set password')}
   end
   context 'when exists license_file_path' do
     let :params do
@@ -124,49 +88,13 @@ describe 'scaleio::cluster' do
     end
     it { is_expected.to contain_exec('scli  --approve_certificate --set_license --license_file /tmp/foo   ').with(
       :command => 'scli  --approve_certificate --set_license --license_file /tmp/foo   ',
-      :path => '/bin/',)}
+      :path => ['/bin/'],)}
     it { is_expected.to contain_notify('SCLI COMMAND: scli  --approve_certificate --set_license --license_file /tmp/foo   ')}
   end
   context 'when license_file_path doesnt exist' do
     let :params do
-      default_params.merge({ :license_file_path => '' })
+      default_params.merge({ :license_file_path => nil })
     end
     it { is_expected.not_to contain_scaleio__cmd('set license')}
-  end
-  context 'when remote_readonly_limit_state is enabled' do
-    let :params do
-      default_params.merge({ :remote_readonly_limit_state => 'enabled' })
-    end
-    it 'sets remote readonly limit state' do
-      is_expected.to contain_scaleio__cmd('set remote readonly limit state').with(
-        :action => 'set',
-        :entity => 'remote_readonly_limit_state',
-        :value => 'enabled')
-    end
-    it { is_expected.to contain_exec('scli  --approve_certificate --set_remote_readonly_limit_state --remote_readonly_limit_state_name enabled   ').with(
-      :command => 'scli  --approve_certificate --set_remote_readonly_limit_state --remote_readonly_limit_state_name enabled   ',
-      :path => '/bin/',)}
-    it { is_expected.to contain_notify('SCLI COMMAND: scli  --approve_certificate --set_remote_readonly_limit_state --remote_readonly_limit_state_name enabled   ')}
-  end
-  context 'when remote_readonly_limit_state is disabled' do
-    let :params do
-      default_params.merge({ :remote_readonly_limit_state => 'disabled' })
-    end
-    it 'sets remote readonly limit state' do
-      is_expected.to contain_scaleio__cmd('set remote readonly limit state').with(
-        :action => 'set',
-        :entity => 'remote_readonly_limit_state', 
-        :value => 'disabled')
-    end
-    it { is_expected.to contain_exec('scli  --approve_certificate --set_remote_readonly_limit_state --remote_readonly_limit_state_name disabled   ').with(
-      :command => 'scli  --approve_certificate --set_remote_readonly_limit_state --remote_readonly_limit_state_name disabled   ',
-      :path => '/bin/',)}
-    it { is_expected.to contain_notify('SCLI COMMAND: scli  --approve_certificate --set_remote_readonly_limit_state --remote_readonly_limit_state_name disabled   ')}
-  end
-  context 'when remote_readonly_limit_state isnt defined' do
-    let :params do
-      default_params.merge({ :remote_readonly_limit_state => '' })
-    end
-    it { is_expected.not_to contain_scaleio__cmd('set remote readonly limit state')}
   end
 end
